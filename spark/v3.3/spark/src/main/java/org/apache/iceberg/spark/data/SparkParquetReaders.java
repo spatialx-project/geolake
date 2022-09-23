f30c9b95672c3ccd9540157a7872e766911b71ed
pengbo <bo.peng@haochezhu.club>
2022-09-23 17:24:57 +0800

    Implemented basic UDFs for geometry values

--- spark/v3.3/spark/src/main/java/org/apache/iceberg/spark/data/SparkParquetReaders.java
+++ spark/v3.3/spark/src/main/java/org/apache/iceberg/spark/data/SparkParquetReaders.java
@@ -808,7 +808,7 @@ public class SparkParquetReaders {
       Binary binary = column.nextBinary();
       ByteBuffer buffer = binary.toByteBuffer();
       Geometry geom = TypeUtil.GeometryUtils.byteBuffer2geometry(buffer);
-      return new GenericArrayData(GeometrySerializer.serialize(geom));
+      return GeometrySerializer.serialize(geom);
     }
   }
 
@@ -823,7 +823,7 @@ public class SparkParquetReaders {
       Binary binary = this.readWKB();
       ByteBuffer buffer = binary.toByteBuffer();
       Geometry geom = TypeUtil.GeometryUtils.byteBuffer2geometry(buffer);
-      return new GenericArrayData(GeometrySerializer.serialize(geom));
+      return GeometrySerializer.serialize(geom);
     }
   }
 
@@ -836,7 +836,7 @@ public class SparkParquetReaders {
     @Override
     public ArrayData read(ArrayData reuse) {
       Geometry geom = this.readGeometry();
-      return new GenericArrayData(GeometrySerializer.serialize(geom));
+      return GeometrySerializer.serialize(geom);
     }
   }
 }
